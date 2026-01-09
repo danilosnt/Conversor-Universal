50d364b1be13977b917cd7416dbf241c2a930e5a
Danilo Santos <daniloddocarmo16@icloud.com>
2026-01-09 18:56:44 -0300

    Add files via upload

---
+++ ConversorUniversal/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ConversorUniversal
+//
+//  Created by Danilo Santos on 09/01/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ConversorUniversal/ConversorUniversalApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConversorUniversalApp.swift
+//  ConversorUniversal
+//
+//  Created by Danilo Santos on 09/01/26.
+//
+
+import SwiftUI
+
+@main
+struct ConversorUniversalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
